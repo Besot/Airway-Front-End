6516962e18ecab30f4f7e9621087eb4df5f33a14
Emmanuel Bobade <emmanuel.bobade@decagon.dev>
2024-03-29 01:17:38 +0100

    This commit contains the forgot password page and component

--- src/Components/ForgetPasswordPage/ForgotPassword.jsx
+++ src/Components/ForgetPasswordPage/ForgotPassword.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import axios from "axios";
+import "./ForgotPassword.css";
+import forgotBg from "/src/assets/Rectangle 58.svg";
+import forgotLogo from "/src/assets/airway_logo.svg";
+import forgotModal from "/src/assets/Vector.svg";
 
-function ForgotPassword() {
+const ForgotPassword = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [status, setStatus] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState({
+    email: "",
+  });
+  const { email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+          "http://localhost:8080/api/v1/auth/forgot-password",
+          user,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+      );
+
+      setShowModal(true);
+      setStatus(true);
+      setSuccessMessage("Password Reset Link Sent!");
+
+      setModalMessage(`Check your email ${email} for the reset link`);
+
+
+    } catch (error) {
+      // Handle the error here
+      console.error("Error during password reset:", error);
+
+      setStatus(false);
+      setErrorMessage("Password Reset Failed!");
+
+      setShowModal(true);
+      setModalMessage(
+          "Kindly check that you have inputted the correct email. If the issue persists, please try again later."
+      );
+    }
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setSuccessMessage("");
+    setErrorMessage("");
+    setModalMessage("");
+  };
+
   return (
-    <div>ForgotPassword</div>
-  )
-}
+      <div className="forget-password-container">
+        <img src={forgotBg} alt="Your Image" className="forget-password-image" />
+        <div className="forget-password-content-container">
+          <div className="password-reset-items">
+            <div className="forgot-password-logo-title">
+              <img className="forgot-password-logo-image" src={forgotLogo}/>
+              <div className="create-acct-text">Reset your password</div>
+            </div>
+            <div className="reset-instruction-form">
+              <div className="reset-password-text">
+                Enter your email below and we’ll send you instructions on how to reset your password.
+              </div>
+              <div className="forgot-password-form">
+                <form onSubmit={(e) => onSubmit(e)}>
+                  <div className="single-input-field">
+                    <label className="forgot-password-label" htmlFor="email">Email Address</label>
+                    <input
+                        className="forgot-password-input-field"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => onInputChange(e)}
+                        placeholder="ChiomaA@gmail.com"
+                        required
+                    />
+                  </div>
+                  <button type="primary" className="forgot-password-button-cta">Send reset instructions</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className={`p-modal-forgot ${showModal ? 'show' : ''}`}>
+            <div className="p-modal-container-forgot">
+              <div className="p-modal-title-forgot">
+                <Modal.Title>
+                  {status ? successMessage : errorMessage}
+                </Modal.Title>
+              </div>
+              <div>
+                <div className="p-icon-forgot">
+                  <img src={forgotModal} alt="Forgot Modal Icon"/>
+                </div>
+                <div className="p-text-body-forgot">
+                  <Modal.Body>
+                    {modalMessage}
+                  </Modal.Body>
+                </div>
+                <Modal.Footer>
+                  <Button
+                      className="p-modal-button-forgot"
+                      onClick={handleClose}>
+                    Continue
+                  </Button>
+                </Modal.Footer>
+              </div>
+            </div>
+          </div>
 
-export default ForgotPassword
+        </div>
+      </div>
+  );
+};
+
+export default ForgotPassword;
