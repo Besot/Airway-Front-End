b84f8fc9d5e1e572333cdd0431f0f64c40999f65
ofektom <okpohot@gmail.com>
2024-03-30 15:47:00 +0100

    Install Vite@latest and onboarding components and pages

--- src/Pages/ForgotPasswordPage.jsx
+++ src/Pages/ForgotPasswordPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import ForgotPassword from '../Components/ForgetPasswordPage copy/ForgotPassword'
 
-function ForgotPasswordPage() {
+const ForgotPasswordPage = () => {
   return (
-    <div>ForgotPasswordPage</div>
+    <ForgotPassword />
   )
 }
 
